001ca7c1dabd1a9e8136f2b49dcb634df7b219d1
cheungth25 <cheungth25@gmail.com>
2017-06-07 16:35:05 -0400

    Add error handler

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,6 +2,24 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+def find_char_data(char_hash, character)
+# look for character and return their data
+  char_hash["results"].select do |char|
+    char["name"].downcase == character
+  end
+end
+
+def find_film_hash(char_data)
+  # grabbing all film URLs for that character
+  film_array = char_data[0]["films"]
+
+  # collect those film API urls, make a web request to each URL to get the info
+  #  for that film
+  film_hash = film_array.each_with_object({}) do |film, hash|
+    hash[film] = JSON.parse(RestClient.get(film))
+  end
+end
+
 def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
@@ -9,26 +27,8 @@ def get_character_movies_from_api(character)
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
 
-  def find_char_data(char_hash, character)
-  # look for character and return their data
-    char_hash["results"].select do |char|
-      char["name"].downcase == character
-    end
-  end
-
-  def find_film_hash(char_data)
-    # grabbing all film URLs for that character
-    film_array = char_data[0]["films"]
-
-    # collect those film API urls, make a web request to each URL to get the info
-    #  for that film
-    film_hash = film_array.each_with_object({}) do |film, hash|
-      hash[film] = JSON.parse(RestClient.get(film))
-    end
-  end
-
   char_data = find_char_data(character_hash, character)
-
+  return if char_data.size == 0
   find_film_hash(char_data)
 end
 
@@ -45,7 +45,11 @@ end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  if films_hash
+    parse_character_movies(films_hash)
+  else
+    puts "This is not the droid you're looking for!"
+  end
 end
 
 ## BONUS
